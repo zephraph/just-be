2c4dd488f8712f89142f2298a0c199b821b19ce6
Justin Bennett <zephraph@gmail.com>
2020-12-28 23:23:07 -0500

    Add lang element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           {process.env.NODE_ENV === "production" && (
             <script
