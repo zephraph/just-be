4c4a2f0a54b672417b7f58e208781e9c9703cb8d
Justin Bennett <zephraph@gmail.com>
2020-12-28 22:54:13 -0500

    Clean up title formatting

--- lib/components/title.tsx
+++ lib/components/title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 }
 
 export const Title = ({ text, date }: TitleProps) => (
-  <header className="notion mb-1 flex items-center justify-between">
+  <header className="notion mb-4">
     <h1 className="notion-h1">{text}</h1>
     {date && <span className="text-gray-600">{formatDate(date)}</span>}
   </header>
