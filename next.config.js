87a98b418558e772639dc3e18614ecf0485f4dcd
Justin Bennett <zephraph@gmail.com>
2021-03-27 01:16:09 -0400

    Further tweak deps

--- next.config.js
+++ next.config.js
@@ -1,42 +1,47 @@
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')(['lodash-es', 'date-fns/esm'])
 const withPreact = require('next-plugin-preact')
 const withNextBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const { default: WindiPlugin } = require('windicss-webpack-plugin')
 
-module.exports = withNextBundleAnalyzer(
-  withPreact({
-    async redirects() {
-      return [
-        {
-          source: '/building-my-blog',
-          destination: '/posts/building-my-blog',
-          permanent: true,
+module.exports = withPlugins([withTM, withPreact, withNextBundleAnalyzer], {
+  async redirects() {
+    return [
+      {
+        source: '/building-my-blog',
+        destination: '/posts/building-my-blog',
+        permanent: true,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/feed',
+        destination: '/api/feed/rss',
+      },
+      {
+        source: '/feed/:slug',
+        destination: '/api/feed/:slug',
+      },
+    ]
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      lodash: 'lodash-es',
+      'date-fns': 'date-fns/esm',
+    }
+    config.plugins.push(
+      new WindiPlugin({
+        scan: {
+          dirs: ['./'],
+          exclude: ['node_modules', '.git', '.next/**/*'],
         },
-      ]
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/feed',
-          destination: '/api/feed/rss',
-        },
-        {
-          source: '/feed/:slug',
-          destination: '/api/feed/:slug',
-        },
-      ]
-    },
-    webpack(config) {
-      config.plugins.push(
-        new WindiPlugin({
-          scan: {
-            dirs: ['./'],
-            exclude: ['node_modules', '.git', '.next/**/*'],
-          },
-        })
-      )
-      return config
-    },
-  })
-)
+      })
+    )
+    return config
+  },
+})
